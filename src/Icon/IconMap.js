63a2e58afe5a09a07c673034e838b42529974dd8
Kristie Giles <kristiegiles@Kristies-MacBook-Pro.local>
2017-09-20 22:11:23 -0700

    removed iconMap

--- src/Icon/IconMap.js
+++
@@ -1,9 +0,0 @@
-const fileList = require.context('./icons', true, /[\s\S]*$/)
-const iconMap = {}
-fileList.keys().forEach(fileName => {
-	fileName = fileName.replace('./', '')
-	iconMap[fileName.replace('.svg', '')] = require(`./icons/${fileName}`)
-});
-
-export default iconMap
-
